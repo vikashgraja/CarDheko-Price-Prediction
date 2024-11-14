e55bf878a7f9db2055c09c19e3da84a525b8f992
Vikash G <vikashgraja@gmail.com>
2024-11-14 13:51:16 +0530

    Updated extract_data

--- extract_data.py
+++ extract_data.py
@@ -1,93 +1,130 @@
 import pandas as pd
 import ast
 
-def parse_json_string(cell_value):
-    try:
-        return ast.literal_eval(cell_value)
-    except (ValueError, SyntaxError):
-        return None
+def extract_new_car_overview(details):
+    if isinstance(details, list):
+        return {df['key']: df['value'] for df in details}
+    else:
+        return {}
 
-def clean_data(df):
-    df['new_car_detail'] = df['new_car_detail'].apply(parse_json_string)
-    df['new_car_overview'] = df['new_car_overview'].apply(parse_json_string)
-    df['new_car_feature'] = df['new_car_feature'].apply(parse_json_string)
-    df['new_car_specs'] = df['new_car_specs'].apply(parse_json_string)
-    
-    df['ignition_type'] = df['new_car_detail'].apply(lambda x: x.get('it') if x else None)
-    df['fuel_type'] = df['new_car_detail'].apply(lambda x: x.get('ft') if x else None)
-    df['body_type'] = df['new_car_detail'].apply(lambda x: x.get('bt') if x else None)
-    df['kilometers_driven'] = df['new_car_detail'].apply(lambda x: x.get('km') if x else None)
-    df['transmission'] = df['new_car_detail'].apply(lambda x: x.get('transmission') if x else None)
-    df['number_of_owners'] = df['new_car_detail'].apply(lambda x: x.get('ownerNo') if x else None)
-    df['owner_details'] = df['new_car_detail'].apply(lambda x: x.get('owner') if x else None)
-    df['oem'] = df['new_car_detail'].apply(lambda x: x.get('oem') if x else None)
-    df['model'] = df['new_car_detail'].apply(lambda x: x.get('model') if x else None)
-    df['model_year'] = df['new_car_detail'].apply(lambda x: x.get('modelYear') if x else None)
-    df['central_variant_id'] = df['new_car_detail'].apply(lambda x: x.get('centralVariantId') if x else None)
-    df['variant_name'] = df['new_car_detail'].apply(lambda x: x.get('variantName') if x else None)
-    df['price'] = df['new_car_detail'].apply(lambda x: x.get('price') if x else None)
-    
-    overview_keys = [
-        'Registration Year', 'Insurance Validity', 'Fuel Type', 'Seats',
-        'Kms Driven', 'RTO', 'Ownership', 'Engine Displacement',
-        'Transmission', 'Year of Manufacture'
-    ]
-    
+def extract_new_car_feature(details):
+    if isinstance(details, list):
+        return {i: df['value'] for i, df in enumerate(details)}
+    else:
+        return {}
 
-    def extract_overview_details(top_list, key_name):
-        if isinstance(top_list, list):
-            for item in top_list:
-                if item.get('key') == key_name:
-                    return item.get('value')
-        return None
-    
-    def extract_feature_data(feature_data, section_name):
-        if isinstance(feature_data, list):
-            for section in feature_data:
-                if section.get('heading') == section_name:
-                    return ', '.join([item['value'] for item in section.get('list', [])])
-        return None
-    
-    def extract_spec_data(spec_data, key_name):
-        if isinstance(spec_data, list):
-            for spec in spec_data:
-                if spec.get('key') == key_name:
-                    return spec.get('value')
-        return None
-    
-    for key in overview_keys:
-        column_name = key.replace(' ', '_').lower()
-        df[column_name] = df['new_car_overview'].apply(
-            lambda x: extract_overview_details(x.get('top') if x else None, key)
-        )
-    
-    df['comfort_features'] = df['new_car_feature'].apply(lambda x: extract_feature_data(x.get('data') if x else None, 'Comfort & Convenience'))
-    df['interior_features'] = df['new_car_feature'].apply(lambda x: extract_feature_data(x.get('data') if x else None, 'Interior'))
-    df['exterior_features'] = df['new_car_feature'].apply(lambda x: extract_feature_data(x.get('data') if x else None, 'Exterior'))
-    df['safety_features'] = df['new_car_feature'].apply(lambda x: extract_feature_data(x.get('data') if x else None, 'Safety'))
-    df['entertainment_features'] = df['new_car_feature'].apply(lambda x: extract_feature_data(x.get('data') if x else None, 'Entertainment & Communication'))
-    
-    
-    spec_keys = ['Engine', 'Max Power', 'Torque', 'Wheel Size', 'Seats']
-    for key in spec_keys:
-        column_name = key.replace(' ', '_').lower()
-        df[column_name] = df['new_car_specs'].apply(lambda x: extract_spec_data(x.get('top') if x else None, key))
-    
-    df_cleaned = df.drop(columns=['new_car_detail', 'new_car_overview', 'new_car_feature', 'new_car_specs','car_links'])
-    
-    return df_cleaned
+
+def extract_new_car_specs(details):
+    if isinstance(details, list):
+        return {df['key']: df['value'] for df in details}
+    else:
+        return {}
+
+
+def convert_binary_cols(df):
+    unique_features = pd.Series(df.values.ravel('K')).dropna().unique()
+    binary_df = pd.DataFrame(0, index=df.index, columns=unique_features)
+
+    for idx, row in df.iterrows():
+        for item in row.dropna():
+            binary_df.at[idx, item] = 1
+    return binary_df
+
+
+def extract_data(data):
+    data.drop(columns=["car_links"], inplace=True, axis=1)
+    data["new_car_detail_dict"] = data["new_car_detail"].apply(ast.literal_eval)
+
+    new_car_detail = pd.json_normalize(data["new_car_detail_dict"])
+
+    data["new_car_overview_dict"] = data["new_car_overview"].apply(ast.literal_eval)
+    new_car_overview = pd.json_normalize(data["new_car_overview_dict"])
+    new_car_overview.drop(columns=["bottomData"], inplace=True, axis=1)
+    new_car_overview = new_car_overview['top'].apply(extract_new_car_overview)
+    new_car_overview = pd.json_normalize(new_car_overview)
+
+    data["new_car_feature_dict"] = data["new_car_feature"].apply(ast.literal_eval)
+
+    new_car_feature = pd.json_normalize(data["new_car_feature_dict"])
+
+    new_car_feature.drop(columns=["commonIcon"], inplace=True, axis=1)
+
+    new_car_feature_top = new_car_feature["top"].apply(extract_new_car_feature)
+    new_car_feature_top = pd.json_normalize(new_car_feature_top)
+
+    new_car_feature_top = convert_binary_cols(new_car_feature_top)
+
+    new_car_feature_all_data = pd.json_normalize(new_car_feature["data"])
+    new_car_feature_extracted = pd.DataFrame()
+    for col in new_car_feature_all_data.columns:
+        new_car_feature_data = pd.json_normalize(new_car_feature_all_data[col])
+        new_car_feature_list = new_car_feature_data["list"].apply(extract_new_car_feature)
+        new_car_feature_list = pd.json_normalize(new_car_feature_list)
+        new_car_feature_list = convert_binary_cols(new_car_feature_list)
+        new_car_feature_extracted = pd.concat([new_car_feature_extracted, new_car_feature_list], axis=1)
+
+    data["new_car_specs_dict"] = data["new_car_specs"].apply(ast.literal_eval)
+
+    new_car_specs = pd.json_normalize(data["new_car_specs_dict"])
+    new_car_specs.drop(columns=["commonIcon"], inplace=True, axis=1)
+
+    new_car_specs_top = new_car_specs["top"].apply(extract_new_car_specs)
+    new_car_specs_top = pd.json_normalize(new_car_specs_top)
+    new_car_specs_data = pd.json_normalize(new_car_specs["data"])
+    new_car_specs_data_extracted = pd.DataFrame()
+    for col in new_car_specs_data.columns:
+        new_car_specs_data_1 = pd.json_normalize(new_car_specs_data[col])
+        new_car_specs_list = new_car_specs_data_1["list"].apply(extract_new_car_specs)
+        new_car_specs_list = pd.json_normalize(new_car_specs_list)
+        new_car_specs_data_extracted = pd.concat([new_car_specs_data_extracted, new_car_specs_list], axis=1)
+
+    extracted_df = pd.concat([new_car_detail, new_car_overview, new_car_specs_top, new_car_specs_data_extracted],
+                             axis=1)
+    return extracted_df
+
+
+def rename_duplicate_columns(df):
+    cols = pd.Series(df.columns)
+    for dup in cols[cols.duplicated()].unique():
+        count = 0
+        for i in range(len(cols)):
+            if cols[i] == dup:
+                count += 1
+                if count > 1:
+                    cols[i] = f"{dup}_{count - 1}"
+    df.columns = cols
+    return df
+
+
+def combined_city(city_data):
+    all_cities_car_data = [rename_duplicate_columns(df) for df in city_data]
+
+    all_columns = set()
+    for df in all_cities_car_data:
+        all_columns.update(df.columns)
+
+    for df in all_cities_car_data:
+        missing_cols = all_columns - set(df.columns)
+        for col in missing_cols:
+            df[col] = pd.NA
+
+    all_cities_car_data = [df.reset_index(drop=True) for df in all_cities_car_data]
+
+    combined_df = pd.concat(all_cities_car_data, axis=0, join='outer', ignore_index=True)
+    return combined_df
 
 if __name__ == "__main__":
-    dataset_list =['bangalore','chennai',
-                   'delhi','hyderabad',
-                   'jaipur','kolkata'
-                   ]
-    master_data = pd.DataFrame()
+    dataset_list = ['bangalore', 'chennai',
+                    'delhi', 'hyderabad',
+                    'jaipur', 'kolkata'
+                    ]
+    city_data = []
 
     for city in dataset_list:
         data = pd.read_excel(f'Dataset/master/{city}_cars.xlsx')
-        df_cleaned = clean_data(data)
+        df_cleaned = extract_data(data)
         df_cleaned['city'] = city
-        master_data = pd.concat([master_data, df_cleaned])
+        city_data += [df_cleaned]
 
-    master_data.to_excel('Dataset/process/extracted_data.xlsx', index=False)
+    combined_df = combined_city(city_data)
+    combined_df.to_excel('Dataset/process/extracted_data.xlsx', index=False)
